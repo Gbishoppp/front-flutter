9d6188ead97c3ee273b4ea69b27c7feaf52ab8d2
gabriel <gabrielsantana115@gmail.com>
2022-10-14 04:50:08 -0300

    layout na appbar

--- lib/main.dart
+++ lib/main.dart
@@ -26,57 +26,16 @@ class Pesquisa extends StatefulWidget {
 }
 
 class _PesquisaState extends State<Pesquisa> {
-  TextEditingController dadoController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: const Text('AppBar',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                ))),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.black12,
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(width: 3),
-              ),
-              child: Column(children: [
-                Stack(
-                  alignment: AlignmentDirectional.centerEnd,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.all(8.0),
-                    ),
-                    TextFormField(
-                      controller: dadoController,
-                      textAlign: TextAlign.center,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'digite o codigo de barras',
-                        fillColor: Colors.white,
-                        filled: true,
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.all(8.0),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Icon(
-                        Icons.search,
-                      ),
-                    )
-                  ],
-                ),
-              ]),
-            ),
-          ),
-        ));
+      appBar: AppBar(
+        title: const Text('Pesquisar'),
+        actions: [
+          IconButton(icon: const Icon(Icons.search), onPressed: () {}),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
